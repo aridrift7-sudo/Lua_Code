8f66ca889be0d04b889a05611dc7f9ce5577d65f
aridrift7-sudo <aridrift7@gmail.com>
2026-04-10 06:29:25 +0700

    Update Percobaan.lua

--- Percobaan.lua
+++ Percobaan.lua
@@ -1,2 +1,7 @@
---VERSION:1
+--VERSION:1.0-beta
+--CHANGELOG_START
+- Perbaikan bug saat navigasi Node
+- Optimasi pemakaian baterai Jieshuo
+- Penambahan fungsi API baru
+--CHANGELOG_END
 service.speak("Ini adalah Node logika Jieshuo versi terbaru dari Tim Ari Joe")
